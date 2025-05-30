f40e63bfa09b1a98d08aedfcde4079f33d308c66
Javier Gutiérrez Medina <147445944+Javier00033@users.noreply.github.com>
2025-05-30 00:56:05 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ npm run build --prod
 
 rm -rf ../static/home/angular_build
 mkdir -p ../static/home/angular_build
-cp -r dist/dist/briefcase_javier_frontend/* ../static/home/angular_build/
+cp -r dist/briefcase_javier_frontend/* ../static/home/angular_build/
